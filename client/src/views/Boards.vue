271e768078de89f191eb4600e7c01217164ee6c7
Jenny <ricejl@gmail.com>
2020-01-06 20:17:37 -0700

    init commit and logout feature

--- client/src/views/Boards.vue
+++ client/src/views/Boards.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="boards">
     WELCOME TO THE BOARDS!!!
+    <button @click="logout">Logout</button>
     <form @submit.prevent="addBoard">
-      <input type="text" placeholder="title" v-model="newBoard.title" required>
-      <input type="text" placeholder="description" v-model="newBoard.description">
+      <input type="text" placeholder="title" v-model="newBoard.title" required />
+      <input type="text" placeholder="description" v-model="newBoard.description" />
       <button type="submit">Create Board</button>
     </form>
     <div v-for="board in boards" :key="board._id">
@@ -13,29 +14,32 @@
 </template>
 
 <script>
-  export default {
-    name: "boards",
-    mounted() {
-      this.$store.dispatch("getBoards");
-    },
-    data() {
-      return {
-        newBoard: {
-          title: "",
-          description: ""
-        }
-      };
-    },
-    computed: {
-      boards() {
-        return this.$store.state.boards;
+export default {
+  name: "boards",
+  mounted() {
+    this.$store.dispatch("getBoards");
+  },
+  data() {
+    return {
+      newBoard: {
+        title: "",
+        description: ""
       }
-    },
-    methods: {
-      addBoard() {
-        this.$store.dispatch("addBoard", this.newBoard);
-        this.newBoard = { title: "", description: "" };
-      }
+    };
+  },
+  computed: {
+    boards() {
+      return this.$store.state.boards;
     }
-  };
+  },
+  methods: {
+    addBoard() {
+      this.$store.dispatch("addBoard", this.newBoard);
+      this.newBoard = { title: "", description: "" };
+    },
+    logout() {
+      this.$store.dispatch("logout");
+    }
+  }
+};
 </script>
